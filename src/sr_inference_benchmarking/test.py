def95a42d3a1f6c9df6c3f728e32e76b80e1dbac
Luis Carretero Lopez <luis.carretero@gmx.de>
2025-01-01 01:52:30 +0100

    Initial PySR+Neural test setup

--- src/sr_inference_benchmarking/test.py
+++ src/sr_inference_benchmarking/test.py
@@ -18,6 +18,14 @@ model = PySRRegressor(
     ],
     precision=64,
     elementwise_loss="loss(prediction, target) = (prediction - target)^2",
+    neural_options=dict(
+        active=True,
+        model_path="/Users/luis/Desktop/Cranmer2024/Workplace/smallMutations/similar-expressions/src/dev/ONNX/onnx-models/model-zwrgtnj0.onnx",
+        sampling_eps=0.01,
+        subtree_min_nodes=5,
+        subtree_max_nodes=10,
+    ),
+    weight_neural_mutate_tree=1.0,
 )
 
 model.fit(X, y)
