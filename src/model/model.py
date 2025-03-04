41904492c5ecf6db8e8611b9f39ddc14345c1c3f
LuisCarretero <luis.carretero@gmx.de>
2025-03-04 14:55:26 +0000

    Added piecewise value contrastive loss

--- src/model/model.py
+++ src/model/model.py
@@ -76,23 +76,23 @@ class LitGVAE(L.LightningModule):
         
         return mean, ln_var, z, logits, values
     
-    def __call__(self, x: torch.Tensor, sample_eps: float, sample_count: int):
-        """
-        Comment this out when creating ONNX model. Causes error when not commenting out during training.
-        Used for final ONNX inference version.
+    # def __call__(self, x: torch.Tensor, sample_eps: float, sample_count: int):
+    #     """
+    #     Comment this out when creating ONNX model. Causes error when not commenting out during training.
+    #     Used for final ONNX inference version.
 
-        shapes
-        x: [batch_cnt, seq_len, token_cnt]
+    #     shapes
+    #     x: [batch_cnt, seq_len, token_cnt]
 
-        """
-        mean, ln_var = self.encoder(x)
-        mean = mean.repeat_interleave(sample_count, dim=0)
-        # ln_var = ln_var.repeat_interleave(sample_count, dim=0)
+    #     """
+    #     mean, ln_var = self.encoder(x)
+    #     mean = mean.repeat_interleave(sample_count, dim=0)
+    #     # ln_var = ln_var.repeat_interleave(sample_count, dim=0)
 
-        self.sampling_eps = sample_eps
-        z = self.sample(mean, torch.zeros_like(ln_var))  # Treating sampling radius as pure hyperparam
-        logits = self.decoder(z)
-        return logits
+    #     self.sampling_eps = sample_eps
+    #     z = self.sample(mean, torch.zeros_like(ln_var))  # Treating sampling radius as pure hyperparam
+    #     logits = self.decoder(z)
+    #     return logits
     
     def training_step(self, batch, batch_idx):
         x, y_syntax, y_consts, y_values = batch
--- src/utils/training.py
+++ src/utils/training.py
@@ -1,5 +1,6 @@
 import math
 import torch
+from torch.nn import functional as F
 import os
 from typing import Dict, List, Tuple
 from omegaconf.dictconfig import DictConfig
@@ -32,12 +33,13 @@ def criterion_factory(cfg: DictConfig, priors: Dict):
     """
     Factory function to create the criterion for the VAE.
     """
-    AE_WEIGHT = cfg.training.criterion.ae_weight
-    CONTRASTIVE_WEIGHT = cfg.training.criterion.contrastive_weight
-    KL_WEIGHT = cfg.training.criterion.kl_weight
-    SYNTAX_WEIGHT = cfg.training.criterion.syntax_weight
-    CONTRASTIVE_SCALE = cfg.training.criterion.contrastive_scale
-    SIMILARITY_THRESHOLD = cfg.training.criterion.similarity_threshold
+    AE_WEIGHT = cfg.training.loss.ae_weight
+    
+    KL_WEIGHT = cfg.training.loss.kl_weight
+    SYNTAX_WEIGHT = cfg.training.loss.syntax_weight
+    ctx = cfg.training.loss.contrastive
+    CONTRASTIVE_WEIGHT, CONTRASTIVE_SCALE, CONTRASTIVE_MODE, CONTRASTIVE_DIMS, SIMILARITY_THRESHOLD = \
+        ctx.weight, ctx.scale, ctx.mode, ctx.dimensions, ctx.similarity_threshold
 
     assert 0 <= AE_WEIGHT <= 1, "AE_WEIGHT must be between 0 and 1"
     assert 0 <= SYNTAX_WEIGHT <= 1, "SYNTAX_WEIGHT must be between 0 and 1"
@@ -46,26 +48,27 @@ def criterion_factory(cfg: DictConfig, priors: Dict):
     assert CONTRASTIVE_SCALE >= 0, "CONTRASTIVE_SCALE must be nonnegative"
     assert SIMILARITY_THRESHOLD >= 0, "SIMILARITY_THRESHOLD must be nonnegative"
 
-    SYNTAX_PRIOR = priors['syntax_prior']
-    CONSTS_PRIOR = priors['consts_prior']
-    VALUES_PRIOR = priors['values_prior']
+    assert cfg.model.io_format.val_points % (CONTRASTIVE_DIMS[1] - CONTRASTIVE_DIMS[0]) == 0, "Value count must be divisible by contrastive dimensions."
+
+    assert CONTRASTIVE_MODE in [None, 'total', 'piecewise'], "CONTRASTIVE_MODE must be one of [None, 'total', 'piecewise']"
+    assert CONTRASTIVE_DIMS is None or len(CONTRASTIVE_DIMS) == 2, "CONTRASTIVE_DIMENSIONS must be a list of two integers"
+
+    SYNTAX_PRIOR, CONSTS_PRIOR, VALUES_PRIOR = priors['syntax_prior'], priors['consts_prior'], priors['values_prior']
 
     cross_entropy = torch.nn.CrossEntropyLoss()  # default reduction is mean over batch and time steps
     mse = torch.nn.MSELoss()
+    z_slice, input_size = calc_zslice(cfg.model.value_decoder.z_slice, cfg.model.z_size)
 
-    # Contrastive loss setup
-    z_slice = cfg.model.value_decoder.z_slice.copy()
-    if z_slice[1] == -1:
-        z_slice[1] = cfg.model.z_size
-    input_size = z_slice[1] - z_slice[0]
-
-    l2_dist_fn = torch.nn.PairwiseDistance(p=2)
-    # m = 1e-1
-    # a = 40  # Sharpness
-    # b = 5  # Shift
-    # gamma_func = lambda x: 1/(1+ torch.exp(a * x - b))
-
-    def criterion(expr_pred: torch.Tensor, values_pred: torch.Tensor, y_rule_idx: torch.Tensor, y_consts: torch.Tensor, y_val: torch.Tensor, kl: float, alpha: float, z: torch.Tensor):
+    def criterion(
+        expr_pred: torch.Tensor, 
+        values_pred: torch.Tensor, 
+        y_rule_idx: torch.Tensor, 
+        y_consts: torch.Tensor, 
+        y_val: torch.Tensor, 
+        kl: float, 
+        alpha: float, 
+        z: torch.Tensor
+    ):
         """
         expr_pred: expression prediction of the model
         values_pred: value prediction of the model
@@ -81,68 +84,93 @@ def criterion_factory(cfg: DictConfig, priors: Dict):
         logits_syntax = expr_pred[:, :, :-1]
         loss_syntax = cross_entropy(logits_syntax.reshape(-1, logits_syntax.size(-1)), y_rule_idx.reshape(-1))/SYNTAX_PRIOR
         loss_consts = mse(expr_pred[:, :, -1], y_consts)/CONSTS_PRIOR
-        loss_recon_ae = SYNTAX_WEIGHT*loss_syntax + (1-SYNTAX_WEIGHT)*loss_consts
+        loss_reconstruction = SYNTAX_WEIGHT*loss_syntax + (1-SYNTAX_WEIGHT)*loss_consts
 
         # VAE total loss (loss_ae = -ELBO = -log p(x|z) + KL_WEIGHT*KL(q(z|x)||p(z)) where KL_WEIGHT is usually denoted as beta)
         kl = torch.tensor(0.0, device=z.device) if KL_WEIGHT == 0 else kl
-        loss_vae = loss_recon_ae + KL_WEIGHT*alpha*kl
+        loss_vae = loss_reconstruction + KL_WEIGHT*alpha*kl
 
         # Value prediction loss
         loss_values = mse(values_pred, y_val)/VALUES_PRIOR
 
-        if CONTRASTIVE_WEIGHT > 0:  # Expensive, so only calculate if necessary
-
-            values_dist = torch.mean((y_val.unsqueeze(1) - y_val.unsqueeze(0))**2, dim=2)
-            similarity_mask = values_dist < SIMILARITY_THRESHOLD
-            
-            u = z[:, z_slice[0]:z_slice[1]]
-            u_dist = l2_dist_fn(u.unsqueeze(1), u.unsqueeze(0))
-            loss_contrastive = torch.sum(similarity_mask * u_dist**2 + (~similarity_mask) * torch.maximum(torch.tensor(0.0, device=z.device), CONTRASTIVE_SCALE - u_dist)**2)
-              # FIXME: Should this be sum or mean? Might get averages later? <- Normalisation should make this invariant under batch size, u-dim and u scale
-              # /torch.sum(u**2)**2
-
-            # Stats for debug
-            # sim_ratio = similarity_mask.float().mean()
-            # sim_count = similarity_mask.sum() - similarity_mask.shape[0]
-            sim_notsame = similarity_mask & (True^torch.eye(similarity_mask.shape[0], device=similarity_mask.device, dtype=torch.bool))
-            u_dist_simnotsame = u_dist[sim_notsame]
-            mean_dist_sim = torch.mean(u_dist_simnotsame)
-            if u_dist_simnotsame.numel() > 0:
-                mean_dist_top_quartile_sim = torch.quantile(u_dist_simnotsame, 0.75)
-                mean_dist_bottom_quartile_sim = torch.quantile(u_dist_simnotsame, 0.25)
-            else:
-                mean_dist_top_quartile_sim = torch.tensor(float('nan'), device=z.device)
-                mean_dist_bottom_quartile_sim = torch.tensor(float('nan'), device=z.device)
-            mean_dist_dissim = torch.mean(u_dist[~similarity_mask])
-
-        else:
-            loss_contrastive = torch.tensor(0.0, device=z.device)
+        # Contrastive loss
+        if CONTRASTIVE_MODE is None or CONTRASTIVE_WEIGHT == 0:
+            loss_contrastive, stats = torch.tensor(0.0, device=z.device), {}
+        elif CONTRASTIVE_MODE == 'total':
+            loss_contrastive, stats = contrastive_loss_total(z[:, z_slice[0]:z_slice[1]], y_val, SIMILARITY_THRESHOLD, CONTRASTIVE_SCALE)
+        elif CONTRASTIVE_MODE == 'piecewise':
+            loss_contrastive, stats = contrastive_loss_piecewise(z[:, CONTRASTIVE_DIMS[0]:CONTRASTIVE_DIMS[1]], y_val, SIMILARITY_THRESHOLD, CONTRASTIVE_SCALE, CONTRASTIVE_DIMS)
 
         # Total loss
-        # loss_vae = torch.tensor(0.0, device=z.device) if AE_WEIGHT == 0 else loss_vae  # FIXME: This is a hack to avoid NaNs in the loss if AE_WEIGHT is 0 (and hence the loss may grow without bound)
         loss = AE_WEIGHT*loss_vae + (1-AE_WEIGHT)*loss_values + CONTRASTIVE_WEIGHT*loss_contrastive
 
         partial_losses = {
             'loss_syntax': loss_syntax.item(),
             'loss_consts': loss_consts.item(),
-            'loss_recon_ae': loss_recon_ae.item(),
+            'loss_recon_ae': loss_reconstruction.item(),
             'kl': kl.item(),
             'alpha': alpha,
             'loss_vae': loss_vae.item(),   # -ELBO but with KL_WEIGHT*alpha so really only some distant cousing of ELBO
             'loss_values': loss_values.item(),
             'loss_contrastive': loss_contrastive.item(),
             'loss': loss.item(),
-            # 'sim_ratio': sim_ratio.item(),
-            # 'sim_count': sim_count.item(),
-            'mean_dist_sim': mean_dist_sim.item(),
-            'mean_dist_dissim': mean_dist_dissim.item(),
-            'mean_dist_top_quartile_sim': mean_dist_top_quartile_sim.item(),
-            'mean_dist_bottom_quartile_sim': mean_dist_bottom_quartile_sim.item(),
-            'dist_ratio': mean_dist_dissim.item() / mean_dist_sim.item()
+            **stats
         }
 
         return loss, partial_losses
     return criterion
+
+def contrastive_loss_total(u: torch.Tensor, y_val: torch.Tensor, similarity_threshold: float, contrastive_scale: float) -> torch.Tensor:
+    values_dist = torch.mean((y_val.unsqueeze(1) - y_val.unsqueeze(0))**2, dim=2)
+    similarity_mask = values_dist < similarity_threshold
+
+    u_dist = F.pairwise_distance(u.unsqueeze(1), u.unsqueeze(0), p=2)  # L2 distance
+    loss_contrastive = torch.mean(similarity_mask * u_dist**2 + (~similarity_mask) * (torch.clamp(contrastive_scale - u_dist, min=0))**2)
+    # TODO: Make this invariant under absolute u scaling: /torch.mean(u**2)**2
+
+    stats = calc_stats_contrastive(u, similarity_mask, u_dist)
+    return loss_contrastive, stats
+    
+def contrastive_loss_piecewise(u: torch.Tensor, y_val: torch.Tensor, similarity_threshold: float, contrastive_scale: float, dimensions: List[int]) -> torch.Tensor:
+    """
+    Split y_val into ndim intervals and calculate similarity within each interval. Then apply contrastive loss to each interval and its corresponding latent space slice.
+    """
+    ndim = dimensions[1] - dimensions[0]
+    y_val = y_val.reshape(y_val.shape[0], ndim, -1)  # [batch_size, intervals, interval_size]
+    values_dist = torch.mean((y_val.unsqueeze(1) - y_val.unsqueeze(0))**2, dim=-1)
+    similarity_mask = values_dist < similarity_threshold
+    
+    # Each distance only along one dimension so L2 reduces to scalar difference
+    u_dist = u.unsqueeze(1) - u.unsqueeze(0)  # [batch_size, batch_size, udim]
+    u_dist_L2 = torch.norm(u_dist, p=2, dim=-1)
+    loss_contrastive_intervals = torch.mean(similarity_mask * u_dist**2 + (~similarity_mask) * (torch.clamp(contrastive_scale - u_dist, min=0))**2, dim=(0, 1))
+    loss_contrastive = torch.mean(loss_contrastive_intervals)  # Mean over intervals
+    
+    stats = calc_stats_contrastive(u, similarity_mask, u_dist_L2)
+    stats['loss_contrastive_intervals'] = loss_contrastive_intervals.item()
+    
+    return loss_contrastive, stats
+
+def calc_stats_contrastive(u: torch.Tensor, similarity_mask: torch.Tensor, u_dist: torch.Tensor) -> Dict[str, float]:
+    # Stats for debug
+    sim_notsame = similarity_mask & (True^torch.eye(similarity_mask.shape[0], device=similarity_mask.device, dtype=torch.bool))
+    u_dist_simnotsame = u_dist[sim_notsame]
+    mean_dist_sim = torch.mean(u_dist_simnotsame)
+    if u_dist_simnotsame.numel() > 0:
+        mean_dist_top_quartile_sim = torch.quantile(u_dist_simnotsame, 0.75)
+        mean_dist_bottom_quartile_sim = torch.quantile(u_dist_simnotsame, 0.25)
+    else:
+        mean_dist_top_quartile_sim = torch.tensor(float('nan'), device=u.device)
+        mean_dist_bottom_quartile_sim = torch.tensor(float('nan'), device=u.device)
+    mean_dist_dissim = torch.mean(u_dist[~similarity_mask])
+    
+    return {
+        'mean_dist_sim': mean_dist_sim.item(),
+        'mean_dist_dissim': mean_dist_dissim.item(),
+        'mean_dist_top_quartile_sim': mean_dist_top_quartile_sim.item(),
+        'mean_dist_bottom_quartile_sim': mean_dist_bottom_quartile_sim.item(),
+        'dist_ratio': mean_dist_dissim.item() / mean_dist_sim.item()
+    }
 
 def compute_latent_metrics(mean: torch.Tensor, ln_var: torch.Tensor) -> Dict[str, float]:
     """
@@ -162,7 +190,6 @@ def calc_syntax_accuracy(logits: torch.Tensor, y_rule_idx: torch.Tensor) -> floa
     y_hat = logits.argmax(-1)
     a = (y_hat == y_rule_idx).float().mean()
     return 100 * a.item()
-
 
 def calc_zslice(z_slice: List[int], z_size: int) -> Tuple[List[int], int]:
     assert len(z_slice) == 2, f"z_slice has to be a list of two integers ({z_slice = })"
